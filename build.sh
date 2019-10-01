f8d2e2f2581dd3f1bdf0beeda90c78e4fd416c08
Jairo Velasco <1904410+jairov4@users.noreply.github.com>
2019-10-01 15:14:02 -0500

    Improve build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
+TAG=$(git describe --dirty --tags)
 $(aws ecr get-login --no-include-email --region us-east-1)
 docker build -t init-aws-swarm .
-docker tag init-aws-swarm:latest 812177567419.dkr.ecr.us-east-1.amazonaws.com/init-aws-swarm:latest
-docker push 812177567419.dkr.ecr.us-east-1.amazonaws.com/init-aws-swarm:latest
+docker tag init-aws-swarm:latest 812177567419.dkr.ecr.us-east-1.amazonaws.com/init-aws-swarm:$TAG
+docker push 812177567419.dkr.ecr.us-east-1.amazonaws.com/init-aws-swarm:$TAG
